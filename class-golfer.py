3b4123ca45bc82e814c219057347ff9ae7e9a569
cheyennewu65 <cheyenne.wu@gmail.com>
2016-08-12 17:52:29 -0700

    Initial creation of golfer class
    Class object, golfer, to manage a player's demographic information.

---
+++ class-golfer.py
@@ -0,0 +1,18 @@
+class Golfer:
+	def __init__(self,fname,mname,lname,pemail,semail):
+		self.__name = list(fname,mname,lname)
+		self.__email = list(pemail,semail)
+		
+	def get_name(self):
+		return self.__name
+		
+	def get_email(self):
+		return self.__email
+		
+	def set_name(self,fname,mname,lname)
+		self.__fname = fname
+		self.__mname = mname
+		self.__lname = lname
+		# call method to update name in database
+		
+	def set_email
